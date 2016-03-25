88f9583ed7524fd990e47f9169d813ef90d4058d
Ihor Chernin <i.chernin@astoundcommerce.com>
2016-03-25 20:29:12 +0200

    Implemented AFFXT2-400: Prepare base order data and shipping/billing info for sending to Affirm  - 400 Extracted data about order from quote in js module. Prepared data for affirm checkout object.

--- OnePica/Affirm/view/frontend/web/js/view/payment/method-renderer/affirm_gateway.js
+++ OnePica/Affirm/view/frontend/web/js/view/payment/method-renderer/affirm_gateway.js
@@ -21,9 +21,10 @@ define(
         'Magento_Checkout/js/view/payment/default',
         'affirmCheckout',
         'Magento_Checkout/js/model/quote',
-        'Magento_Checkout/js/model/payment/additional-validators'
+        'Magento_Checkout/js/model/payment/additional-validators',
+        'mage/url'
     ],
-    function (Component, AffirmCheckout, quote, additionalValidator) {
+    function (Component, AffirmCheckout, quote, additionalValidator, url) {
         'use strict';
 
         return Component.extend({
@@ -32,16 +33,81 @@ define(
                 transactionResult: ''
             },
 
+            /**
+             * Payment code
+             *
+             * @returns {string}
+             */
             getCode: function () {
                 return 'affirm_gateway';
             },
 
             /**
-             * @override
+             * After place order handler
              */
-            placeOrder: function () {
+            afterPlaceOrder: function () {
+                var totals = quote.getTotals()(),
+                    checkoutData = {},
+                    billingData = quote.billingAddress(),
+                    shippingData = quote.shippingAddress(),
+                    shippingMethod = quote.shippingMethod(),
+                    quoteItems = quote.getItems();
+
+                // Create checkout data object
+                checkoutData.merchant = {};
+                checkoutData.merchant.user_confirmation_url = window.checkoutConfig.payment['affirm_gateway'].merchant.confirmationUrl;
+                checkoutData.merchant.user_cancel_url = window.checkoutConfig.payment['affirm_gateway'].merchant.cancelUrl;
+
+                checkoutData.config = {};
+                checkoutData.config.financial_product_key =  window.checkoutConfig.payment['affirm_gateway'].config.financialKey;
+
+                checkoutData.shipping = {};
+                checkoutData.shipping.name = {};
+                checkoutData.shipping.name.full = shippingData.firstname + ' ' + shippingData.lastname;
+
+                checkoutData.shipping.address = {};
+                checkoutData.shipping.address.line1 = shippingData.street[0] + ' ' + shippingData.street[1];
+
+                checkoutData.shipping.address.city = shippingData.city;
+                checkoutData.shipping.address.state = shippingData.regionCode;
+                checkoutData.shipping.address.zipcode = shippingData.postcode;
+                checkoutData.shipping.address.country = "USA";
+
+                //Specify billing data
+                checkoutData.billing = {};
+                checkoutData.billing.name = {};
+                checkoutData.billing.name.full = billingData.firstname + ' ' + billingData.lastname;
+                checkoutData.billing.address = {};
+                checkoutData.billing.address.line1 = billingData.street[0] + ' ' + billingData.street[1];
+
+                checkoutData.billing.address.city = billingData.city;
+                checkoutData.billing.address.state = billingData.regionCode;
+                checkoutData.billing.address.zipcode = billingData.postcode;
+                checkoutData.billing.address.country = "USA";
+                checkoutData.items = [];
+
+                for (var i = 0; i < quoteItems.length; i++) {
+                    checkoutData.items[i] = {};
+                    checkoutData.items[i].display_name = quoteItems[i].name;
+                    checkoutData.items[i].sku = quoteItems[i].sku;
+                    checkoutData.items[i].unit_price = parseInt(quoteItems[i].price * 100);
+                    checkoutData.items[i].qty = quoteItems[i].qty;
+                    checkoutData.items[i].item_image_url = quoteItems[i].thumbnail;
+                    checkoutData.items[i].item_url = url.build(quoteItems[i].product.request_path);
+                }
+                checkoutData.discounts = {};
+                checkoutData.metadata = {};
+
+                checkoutData.metadata.shipping_type = shippingMethod.carrier_title;
+                checkoutData.order_id = "#";
+                checkoutData.total = totals.grand_total * 100;
+                checkoutData.shipping_amount = totals.base_shipping_amount;
+                checkoutData.tax_amount = totals.base_tax_amount;
+
                 if (additionalValidator.validate()) {
-                    //TODO: we should send request from there
+                    // setup and configure checkout
+                    affirm.checkout(checkoutData);
+                    affirm.checkout.post();
                 }
             }
         });
