158d4a4b502f12f981bf85a3c5e3ce67f13e54bc
Ihor Chernin <i.chernin@astoundcommerce.com>
2016-03-28 11:24:31 +0300

    Implemented AFFXT2-117: Implement redirect on Affirm side after placing order  - 117 Reconfigured payment js for sending data to the Affirm.

--- OnePica/Affirm/view/frontend/web/js/view/payment/method-renderer/affirm_gateway.js
+++ OnePica/Affirm/view/frontend/web/js/view/payment/method-renderer/affirm_gateway.js
@@ -46,68 +46,156 @@ define(
              * After place order handler
              */
             afterPlaceOrder: function () {
-                var totals = quote.getTotals()(),
-                    checkoutData = {},
-                    billingData = quote.billingAddress(),
-                    shippingData = quote.shippingAddress(),
-                    shippingMethod = quote.shippingMethod(),
-                    quoteItems = quote.getItems();
+                var checkoutData = {};
 
-                // Create checkout data object
-                checkoutData.merchant = {};
-                checkoutData.merchant.user_confirmation_url = window.checkoutConfig.payment['affirm_gateway'].merchant.confirmationUrl;
-                checkoutData.merchant.user_cancel_url = window.checkoutConfig.payment['affirm_gateway'].merchant.cancelUrl;
-
-                checkoutData.config = {};
-                checkoutData.config.financial_product_key =  window.checkoutConfig.payment['affirm_gateway'].config.financialKey;
-
-                checkoutData.shipping = {};
-                checkoutData.shipping.name = {};
-                checkoutData.shipping.name.full = shippingData.firstname + ' ' + shippingData.lastname;
-
-                checkoutData.shipping.address = {};
-                checkoutData.shipping.address.line1 = shippingData.street[0] + ' ' + shippingData.street[1];
-
-                checkoutData.shipping.address.city = shippingData.city;
-                checkoutData.shipping.address.state = shippingData.regionCode;
-                checkoutData.shipping.address.zipcode = shippingData.postcode;
-                checkoutData.shipping.address.country = "USA";
-
-                //Specify billing data
-                checkoutData.billing = {};
-                checkoutData.billing.name = {};
-                checkoutData.billing.name.full = billingData.firstname + ' ' + billingData.lastname;
-                checkoutData.billing.address = {};
-                checkoutData.billing.address.line1 = billingData.street[0] + ' ' + billingData.street[1];
-
-                checkoutData.billing.address.city = billingData.city;
-                checkoutData.billing.address.state = billingData.regionCode;
-                checkoutData.billing.address.zipcode = billingData.postcode;
-                checkoutData.billing.address.country = "USA";
-                checkoutData.items = [];
-
-                for (var i = 0; i < quoteItems.length; i++) {
-                    checkoutData.items[i] = {};
-                    checkoutData.items[i].display_name = quoteItems[i].name;
-                    checkoutData.items[i].sku = quoteItems[i].sku;
-                    checkoutData.items[i].unit_price = parseInt(quoteItems[i].price * 100);
-                    checkoutData.items[i].qty = quoteItems[i].qty;
-                    checkoutData.items[i].item_image_url = quoteItems[i].thumbnail;
-                    checkoutData.items[i].item_url = url.build(quoteItems[i].product.request_path);
-                }
-                checkoutData.discounts = {};
-                checkoutData.metadata = {};
-
-                checkoutData.metadata.shipping_type = shippingMethod.carrier_title;
-                checkoutData.order_id = "#";
-                checkoutData.total = totals.grand_total * 100;
-                checkoutData.shipping_amount = totals.base_shipping_amount;
-                checkoutData.tax_amount = totals.base_tax_amount;
+                // Init all data for the sending request
+                this.initBillingData(checkoutData);
+                this.initShippingData(checkoutData);
+                this.initMerchantData(checkoutData);
+                this.initConfigData(checkoutData);
+                this.initAdditionalData(checkoutData);
+                this.initItems(checkoutData);
+                this.initCheckoutTotals(checkoutData);
 
                 if (additionalValidator.validate()) {
                     // setup and configure checkout
                     affirm.checkout(checkoutData);
                     affirm.checkout.post();
+                }
+            },
+
+            /**
+             * Init merchant Data
+             * @param checkoutDataObject
+             */
+            initMerchantData: function(checkoutDataObject) {
+                var confUrl = window.checkoutConfig.payment['affirm_gateway'].merchant.confirmationUrl,
+                    cancelUrl = window.checkoutConfig.payment['affirm_gateway'].merchant.confirmationUrl;
+                // Specify merchant data
+                checkoutDataObject.merchant = checkoutDataObject.merchant || {};
+                checkoutDataObject.merchant.user_confirmation_url = confUrl;
+                checkoutDataObject.merchant.user_cancel_url = cancelUrl;
+            },
+
+            /**
+             * Init config data
+             * @param checkoutDataObject
+             */
+            initConfigData: function(checkoutDataObject) {
+                var financialKey = window.checkoutConfig.payment['affirm_gateway'].config.financialKey;
+                checkoutDataObject.config = checkoutDataObject.config || {};
+                checkoutDataObject.config.financial_product_key = financialKey;
+            },
+
+            /**
+             * Init quote shipping data
+             * @param checkoutDataObject
+             */
+            initShippingData: function(checkoutDataObject) {
+                var shippingData = quote.shippingAddress();
+                checkoutDataObject.shipping = checkoutDataObject.shipping || {};
+                checkoutDataObject.shipping.name = checkoutDataObject.shipping.name || {};
+                if (shippingData.lastname) {
+                    checkoutDataObject.shipping.name.full = shippingData.firstname + shippingData.lastname;
+                } else {
+                    checkoutDataObject.shipping.name.full = shippingData.firstname;
+                }
+                checkoutDataObject.shipping.address = checkoutDataObject.shipping.address || {};
+                if (shippingData.street[1]) {
+                    checkoutDataObject.shipping.address.line1 = shippingData.street[0] + shippingData.street[1];
+                } else {
+                    checkoutDataObject.shipping.address.line1 = shippingData.street[0];
+                }
+                if (shippingData.city) {
+                    checkoutDataObject.shipping.address.city = shippingData.city;
+                }
+                if (shippingData.regionCode) {
+                    checkoutDataObject.shipping.address.state = shippingData.regionCode;
+                }
+                if (shippingData.postcode) {
+                    checkoutDataObject.shipping.address.zipcode = shippingData.postcode;
+                }
+                checkoutDataObject.shipping.address.country = "USA";
+            },
+
+            /**
+             * Init billing data
+             * @param checkoutDataObject
+             */
+            initBillingData: function (checkoutDataObject)
+            {
+                var billingData = quote.billingAddress();
+                checkoutDataObject.billing = checkoutDataObject.billing || {};
+                checkoutDataObject.billing.name = checkoutDataObject.billing.name || {};
+                if (billingData.lastname) {
+                    checkoutDataObject.billing.name.full = billingData.firstname + billingData.lastname;
+                } else {
+                    checkoutDataObject.billing.name.full = billingData.firstname;
+                }
+                checkoutDataObject.billing.address = checkoutDataObject.billing.address || {};
+                if (billingData.street[1]) {
+                    checkoutDataObject.billing.address.line1 = billingData.street[0] + billingData.street[1];
+                } else {
+                    checkoutDataObject.billing.address.line1 = billingData.street[0];
+                }
+                if (billingData.city) {
+                    checkoutDataObject.billing.address.city = billingData.city;
+                }
+                if (billingData.regionCode) {
+                    checkoutDataObject.billing.address.state = billingData.regionCode;
+                }
+                if (billingData.postcode) {
+                    checkoutDataObject.billing.address.zipcode = billingData.postcode;
+                }
+                checkoutDataObject.billing.address.country = "USA";
+            },
+
+            /**
+             * Init quote items
+             *
+             * @param checkoutDataObject
+             */
+            initItems: function(checkoutDataObject) {
+                var quoteItems = quote.getItems();
+                checkoutDataObject.items = checkoutDataObject.items || [];
+                for (var i = 0; i < quoteItems.length; i++) {
+                    checkoutDataObject.items[i] = {};
+                    checkoutDataObject.items[i].display_name = quoteItems[i].name;
+                    checkoutDataObject.items[i].sku = quoteItems[i].sku;
+                    checkoutDataObject.items[i].unit_price = parseInt(quoteItems[i].price * 100);
+                    checkoutDataObject.items[i].qty = quoteItems[i].qty;
+                    checkoutDataObject.items[i].item_image_url = quoteItems[i].thumbnail;
+                    checkoutDataObject.items[i].item_url = url.build(quoteItems[i].product.request_path);
+                }
+            },
+
+            /**
+             * Init totals with converts to cents
+             *
+             * @param checkoutDataObject
+             */
+            initCheckoutTotals: function (checkoutDataObject) {
+                var totals = quote.getTotals()();
+                checkoutDataObject.total = totals.grand_total * 100;
+                checkoutDataObject.shipping_amount = totals.base_shipping_amount * 100;
+                checkoutDataObject.tax_amount = totals.base_tax_amount * 100;
+            },
+
+            /**
+             * Init discounts metadata and others.
+             *
+             * @param checkoutDataObject
+             */
+            initAdditionalData: function (checkoutDataObject) {
+                var shippingMethod = quote.shippingMethod();
+
+                checkoutDataObject.discounts = checkoutDataObject.discounts || {};
+                checkoutDataObject.metadata = checkoutDataObject.discounts || {};
+
+                //TODO: Now this value just hardcoded, need to find a way to get this info from quote object
+                checkoutDataObject.order_id = "#";
+                if (shippingMethod.carrier_title) {
+                    checkoutDataObject.metadata.shipping_type = shippingMethod.carrier_title;
                 }
             }
         });
