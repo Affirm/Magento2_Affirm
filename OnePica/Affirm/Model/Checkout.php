26e14eceb272f5ad4283b303b4f78592e216fc07
Ihor Chernin <i.chernin@astoundcommerce.com>
2016-04-05 14:49:47 +0300

    Implemented AFFXT2-416: Implement saving response data  - 416 - Added license description to checkout object.

--- OnePica/Affirm/Model/Checkout.php
+++ OnePica/Affirm/Model/Checkout.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * OnePica
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to codemaster@onepica.com so we can send you a copy immediately.
+ *
+ * @category  Affirm
+ * @package   OnePica_Affirm
+ * @copyright Copyright (c) 2016 One Pica, Inc. (http://www.onepica.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
 namespace OnePica\Affirm\Model;
 
 use \Magento\Quote\Api\CartManagementInterface;
