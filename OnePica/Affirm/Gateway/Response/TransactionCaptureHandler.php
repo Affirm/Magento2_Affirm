5a783d67c7663cdca5214ce7a9b5336f1eb17c2a
Sergii Shkiria <s.shkiria@astoundcommerce.com>
2016-04-12 17:04:15 +0300

    Implemented AFFXT2-406: Add new capture transaction in Transactions list

---
+++ OnePica/Affirm/Gateway/Response/TransactionCaptureHandler.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * OnePica
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to codemaster@onepica.com so we can send you a copy immediately.
+ *
+ * @category  Affirm
+ * @package   OnePica_Affirm
+ * @copyright Copyright (c) 2016 One Pica, Inc. (http://www.onepica.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace OnePica\Affirm\Gateway\Response;
+
+use Magento\Sales\Model\Order\Payment;
+use Magento\Payment\Gateway\Helper\SubjectReader;
+use Magento\Payment\Gateway\Response\HandlerInterface;
+
+/**
+ * Class TransactionCaptureHandler
+ */
+class TransactionCaptureHandler implements HandlerInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function handle(array $handlingSubject, array $response)
+    {
+        $paymentDO = SubjectReader::readPayment($handlingSubject);
+        /** @var Payment $orderPayment */
+        $orderPayment = $paymentDO->getPayment();
+        $orderPayment->setIsTransactionClosed(false);
+    }
+}
