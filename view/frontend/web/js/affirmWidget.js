8273198f8bc5e4ad0ef48bed0f3b6a514b6f20f4
ant-workaholic <igorchernin@yahoo.com>
2016-06-10 13:14:19 +0300

    Fixed AFFXT2-517: Affirm promo sections are duplicating on PDP and Cart page - Fixed affirm script loading issue.

--- view/frontend/web/js/affirmWidget.js
+++ view/frontend/web/js/affirmWidget.js
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2015 Fastgento. All rights reserved.
+ * Copyright © 2016 Astound. All rights reserved.
  * See COPYING.txt for license details.
  */
 /*jshint jquery:true*/
@@ -9,36 +9,37 @@ define(["jquery",
     "Astound_Affirm/js/model/aslowas"
 ], function ($, $t) {
     "use strict"
-
     $.widget('mage.affirmWidget',{
         options: {},
         _create: function() {
-            (function(l, g, m, e, a, f, b) {
-                var d, c = l[m] || {},
-                    h = document.createElement(f),
-                    n = document.getElementsByTagName(f)[0],
-                    k = function(a, b, c) {
+            if (typeof affirm == "undefined") {
+                (function(l, g, m, e, a, f, b) {
+                    var d, c = l[m] || {},
+                        h = document.createElement(f),
+                        n = document.getElementsByTagName(f)[0],
+                        k = function(a, b, c) {
                         return function() {
                             a[b]._.push([c, arguments])
                         }
                     };
-                c[e] = k(c, e, "set");
-                d = c[e];
-                c[a] = {};
-                c[a]._ = [];
-                d._ = [];
-                c[a][b] = k(c, a, b);
-                a = 0;
-                for (b = "set add save post open empty reset on off trigger ready setProduct".split(" "); a < b.length; a++) d[b[a]] = k(c, e, b[a]);
-                a = 0;
-                for (b = ["get", "token", "url", "items"]; a < b.length; a++) d[b[a]] = function() {};
-                h.async = !0;
-                h.src = g[f];
-                n.parentNode.insertBefore(h, n);
-                delete g[f];
-                d(g);
-                l[m] = c
-            })(window, this.options, "affirm", "checkout", "ui", "script", "ready");
+                    c[e] = k(c, e, "set");
+                    d = c[e];
+                    c[a] = {};
+                    c[a]._ = [];
+                    d._ = [];
+                    c[a][b] = k(c, a, b);
+                    a = 0;
+                    for (b = "set add save post open empty reset on off trigger ready setProduct".split(" "); a < b.length; a++) d[b[a]] = k(c, e, b[a]);
+                    a = 0;
+                    for (b = ["get", "token", "url", "items"]; a < b.length; a++) d[b[a]] = function() {};
+                    h.async = !0;
+                    h.src = g[f];
+                    n.parentNode.insertBefore(h, n);
+                    delete g[f];
+                    d(g);
+                    l[m] = c
+                })(window, this.options, "affirm", "checkout", "ui", "script", "ready");
+            }
         }
     });
     return $.mage.affirmWidget
