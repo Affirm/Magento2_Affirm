fb6efc2a4b93bfe3609d95486ef3d0697c449d27
Sergii Shkiria <s.shkiria@astoundcommerce.com>
2016-07-15 10:11:22 +0300

    Implemented AFFXT2-558: Implement saving affirm fpid value for customer. Implemented.

---
+++ Model/Observer/IdentifyFinancingProgram.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Astound
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to codemaster@astoundcommerce.com so we can send you a copy immediately.
+ *
+ * @category  Affirm
+ * @package   Astound_Affirm
+ * @copyright Copyright (c) 2016 Astound, Inc. (http://www.astoundcommerce.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Astound\Affirm\Model\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+use Magento\Customer\Model\Session;
+
+/**
+ * Identify Financing Program for customer
+ */
+class IdentifyFinancingProgram implements ObserverInterface
+{
+    /**
+     * Init
+     *
+     */
+    public function __construct(
+        Session $customerSession
+    ) {
+        $this->_customerSession = $customerSession;
+    }
+
+    /**
+     * Execute
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return $this|void
+     */
+    public function execute(Observer $observer)
+    {
+        $controller = $observer->getControllerAction();
+        $financingProgramValue = $controller->getRequest()->getParam('affirm_fpid');
+        if (empty($financingProgramValue)) {
+            return;
+        }
+        if ($this->_customerSession->isLoggedIn()) {
+            $this->_updateLoggedInCustomerMFP($financingProgramValue);
+        } else {
+            $this->_updateGuestCustomerMFP($financingProgramValue);
+        }
+        return $this;
+    }
+
+    /**
+     * Update for logged-in customer
+     *
+     * @param string $financingProgramValue
+     */
+    protected function _updateLoggedInCustomerMFP($financingProgramValue)
+    {
+        $customer = $this->_customerSession->getCustomer();
+        $customerMFPValue = $customer->getAffirmCustomerMfp();
+        if (empty($customerMFPValue) || ($financingProgramValue != $customerMFPValue)) {
+            $customer->setAffirmCustomerMfp($financingProgramValue);
+            $customer->save();
+            //in case if customer logout to keep actual value during session
+            $this->_updateGuestCustomerMFP($financingProgramValue);
+        }
+    }
+
+    /**
+     * Update for guest
+     *
+     * @param string $financingProgramValue
+     */
+    protected function _updateGuestCustomerMFP($financingProgramValue)
+    {
+        $sessionMFPValue = $this->_customerSession->getAffirmCustomerMfp();
+        if (empty($sessionMFPValue) || ($financingProgramValue != $sessionMFPValue)) {
+            $this->_customerSession->setAffirmCustomerMfp($financingProgramValue);
+        }
+    }
+}
---
+++ Model/Observer/UpdateFinancingProgramOnLogin.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Astound
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to codemaster@astoundcommerce.com so we can send you a copy immediately.
+ *
+ * @category  Affirm
+ * @package   Astound_Affirm
+ * @copyright Copyright (c) 2016 Astound, Inc. (http://www.astoundcommerce.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Astound\Affirm\Model\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+use Magento\Customer\Model\Session;
+
+/**
+ * Update Financing Program for customer on login
+ */
+class UpdateFinancingProgramOnLogin implements ObserverInterface
+{
+    /**
+     * Init
+     *
+     */
+    public function __construct(
+        Session $customerSession
+    ) {
+        $this->_customerSession = $customerSession;
+    }
+
+    /**
+     * Execute
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return $this|void
+     */
+    public function execute(Observer $observer)
+    {
+        $sessionFinancingProgramValue = $this->_customerSession->getAffirmCustomerMfp();
+        if ($this->_customerSession->isLoggedIn()) {
+            $customer = $observer->getCustomer();
+            if (!empty($sessionFinancingProgramValue) &&
+                ($customer->getAffirmCustomerMfp() != $sessionFinancingProgramValue)
+            ) {
+                $customer->setAffirmCustomerMfp($sessionFinancingProgramValue);
+                $customer->save();
+            }
+        }
+        return $this;
+    }
+}
