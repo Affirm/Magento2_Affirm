42d5bccb8ef8d068241faec14be9c833d13bed09
Nanda <nanda.dubey@affirm.com>
2019-03-29 11:50:54 -0700

    Revert "2.3.0 version support code with backward compatibility"
    This reverts commit c30da893dd9fc2b809dd2cf8cac762f5a7d7d8ba.

--- Controller/Payment/Extendable/Main.php
+++ Controller/Payment/Extendable/Main.php
@@ -1,37 +1,20 @@
 <?php
 
 /**
+ * Astound
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to codemaster@astoundcommerce.com so we can send you a copy immediately.
  *
- *  * BSD 3-Clause License
- *  *
- *  * Copyright (c) 2018, Affirm
- *  * All rights reserved.
- *  *
- *  * Redistribution and use in source and binary forms, with or without
- *  * modification, are permitted provided that the following conditions are met:
- *  *
- *  *  Redistributions of source code must retain the above copyright notice, this
- *  *   list of conditions and the following disclaimer.
- *  *
- *  *  Redistributions in binary form must reproduce the above copyright notice,
- *  *   this list of conditions and the following disclaimer in the documentation
- *  *   and/or other materials provided with the distribution.
- *  *
- *  *  Neither the name of the copyright holder nor the names of its
- *  *   contributors may be used to endorse or promote products derived from
- *  *   this software without specific prior written permission.
- *  *
- *  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- *  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- *  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- *  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- *  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * @category  Affirm
+ * @package   Astound_Affirm
+ * @copyright Copyright (c) 2016 Astound, Inc. (http://www.astoundcommerce.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 namespace Astound\Affirm\Controller\Payment\Extendable;
@@ -45,7 +28,7 @@ use Astound\Affirm\Model\Checkout;
 use Magento\Framework\Exception\LocalizedException;
 
 /**
- * Class Main
+ * Class Confirm
  *
  * @package Astound\Affirm\Controller\Payment\Extendable
  */
