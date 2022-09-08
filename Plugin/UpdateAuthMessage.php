e58b77f84764d67491296d874f08ba0290d237d7
Tae Lim <tae.lim@affirm.com>
2022-09-08 16:59:16 -0700

    docblock

--- Plugin/UpdateAuthMessage.php
+++ Plugin/UpdateAuthMessage.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Astound\Affirm\Plugin;
-/**/
 
 use Magento\Framework\Pricing\Helper\Data;
 use Magento\Sales\Model\Order\Payment\State\AuthorizeCommand;
@@ -10,14 +9,37 @@ use Magento\Sales\Api\Data\OrderPaymentInterface;
 
 class UpdateAuthMessage
 {
+    /**
+     * @var Data
+     */
     protected $priceHelper;
 
-    public function __construct(Data $priceHelper) {
+    /**
+     * @param Data $priceHelper
+     */
+    public function __construct(
+        Data $priceHelper
+    ) {
         $this->priceHelper = $priceHelper;
     }
 
-    public function afterExecute(AuthorizeCommand $subject, $result, OrderPaymentInterface $payment, $amount, OrderInterface $order)
-    {
+    /**
+     * Updates order comment message with current store currency amount
+     *
+     * @param AuthorizeCommand $subject
+     * @param $result
+     * @param OrderPaymentInterface $payment
+     * @param $amount
+     * @param OrderInterface $order
+     * @return \Magento\Framework\Phrase
+     */
+    public function afterExecute(
+        AuthorizeCommand $subject,
+        $result,
+        OrderPaymentInterface $payment,
+        $amount,
+        OrderInterface $order
+    ) {
         $order_currency_code = $order->getOrderCurrencyCode();
         $new_amount = $this->priceHelper->currency($amount, true, false);
         $_message = __($result->getText(), $order_currency_code . " " . $new_amount);
