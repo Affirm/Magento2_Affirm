9f3cae1afcce443f59c4f99577c37b35a8104831
Dimitri BOUTEILLE <34821762+dimitriBouteille@users.noreply.github.com>
2024-12-20 20:00:42 +0100

    GraphQl : Add Affirm store config (#138)
    Co-authored-by: Dimitri BOUTEILLE <dimitri.bouteille@reflet-digital.com>

---
+++ Model/Resolver/StoreConfig/ApiKey.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Astound
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to codemaster@astoundcommerce.com so we can send you a copy immediately.
+ *
+ * @category  Affirm
+ * @package   Astound_Affirm
+ * @copyright Copyright (c) 2016 Astound, Inc. (http://www.astoundcommerce.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Astound\Affirm\Model\Resolver\StoreConfig;
+
+use Astound\Affirm\Model\Config;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+/**
+ * Class ApiKey
+ *
+ * @package Astound\Affirm\Model\Resolver\StoreConfig
+ */
+class ApiKey implements ResolverInterface
+{
+    public Config $config;
+
+    /**
+     * ApiKey Constructor
+     *
+     * @param \Astound\Affirm\Model\Config $config
+     */
+    public function __construct(
+        Config $config
+    ) {
+        $this->config = $config;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        return $this->config->getPublicApiKey();
+    }
+}
